3d02bf51adf6ea9575ca984a4dd593bf8652f4e2
shwon <kairise1234@gmail.com>
2025-12-20 10:29:44 +0900

    chore: http 임시 도메인 허가

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**", // 이미지 테스트를 위한 외부 이미지 도메인 허용
       },
+      {
+        protocol: "http",
+        hostname: "localhost", // 이미지 테스트를 위한 임시 이미지 도메인 허용
+        port: "3001",
+      },
     ],
   },
 };
