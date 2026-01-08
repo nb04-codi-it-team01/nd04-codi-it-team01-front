357768ec5568a898f4c2174e47aab5093532befd
shwon <kairise1234@gmail.com>
2026-01-08 13:29:54 +0900

    fix: next.config.ts에 s3 도메인 명시

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**", // 이미지 테스트를 위한 외부 이미지 도메인 허용
+        hostname: "codiit-team1-images.s3.ap-northeast-2.amazonaws.com", // S3 버킷의 정확한 호스트네임
+        port: "",
+        pathname: "/**", // 버킷 내 모든 경로 허용
       },
       {
         protocol: "http",
